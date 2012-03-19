41acb844e102cbaf022882b02f23e4f12e3c0ad1
unknown <kadeem@kadeem-PC.(none)>
2012-03-19 00:41:32 -0400

    Sql Dump Addition

--- models/public_request.php
+++ models/public_request.php
@@ -1,19 +1,71 @@
 <?php
-/*
-*Model that connects to the Public_Request table in the database.
-*@Author Marc Byfield
-*/
-
 require_once 'php-activerecord/ActiveRecord.php';
- 
+
 ActiveRecord\Config::initialize(function($cfg)
- {
-     $cfg->set_model_directory('models');
-     $cfg->set_connections(array(
-         'development' => 'mysql://C344630_infantry:INFANTRY321@66.116.150.176/C344630_streetlight'));
-          });
+{
+	 $cfg->set_model_directory('models');
+	 $cfg->set_connections(array(
+	 //'development' => 'mysql://C344630_infantry:INFANTRY321@66.116.150.176/C344630_streetlight'));
+	//'development' => 'mysql://username:password@localhost/database_name'));
+	'development' => 'mysql://root@localhost/streetlight'));
+});
 
-class Public_Request extends ActiveRecord\Model 
-{}
+class Public_Request extends ActiveRecord\Model
+{
+	var $referenceNumber = 000000; //class variable instantiated to a default value.currently not being used
+	
+	//save update, delete  and most retrieve methods will be handled by the active record
+	
+	function generate_Reference()
+	{
+		$currentDateTime = getdate();
+		$year = substr($currentDateTime['year'],-2);
+		$month = $currentDateTime['mon'];
+		$day = $currentDateTime['mday'];
+		$hours = $currentDateTime['hours'];
+		$minutes = $currentDateTime['minutes'];
+		
+		$referenceNo = $year."".$month."".$day."".$hours."".$minutes;
+	
+		$existingRequest = Public_Request::find("reference_no = $referenceNo"); 
+		
+		While ($existingRequest != null)
+		{			
+			$currentDateTime = getdate();
+			$year = substr($currentDateTime['year'],-2);
+			$month = $currentDateTime['mon'];
+			$day = $currentDateTime['mday'];
+			$hours = $currentDateTime['hours'];
+			$minutes = $currentDateTime['minutes'];
+			$minutes++;
+			$referenceNo = $year."".$month."".$day."".$hours."".$minutes;
 
+			$existingRequest = Public_Request::find("reference_no = $referenceNo"); 
+		}
+		
+		return $referenceNo;
+	}
+	
+	
+	function retrieve_PublicRequestbyReference($referenceNo)
+	{
+		$requestInfo = Public_Request::find("reference_no = $referenceNo"); // retrieves a particular request information and stores the result into an array.
+		
+		return $requestInfo;
+	}
+	
+	function retrieve_UserRequests($userId)
+	{
+		$requests = Public_Request::find("userid = $userId"); // retrieves a particular user's requests and stores the result into an array.
+		
+		return $requests;
+	}
+	
+	function retrieve_AllRequests($userId)
+	{
+		$requests = Public_Request::find(); // retrieves all requests and stores the result into an array.
+		
+		return $requests;
+	}
+}
 ?>
--- test-kad.php
+++ test-kad.php
@@ -1,49 +1,7 @@
 <?php
-require_once 'php-activerecord/ActiveRecord.php';
+require 'models/public_request.php';
 
-ActiveRecord\Config::initialize(function($cfg)
-{
-	$cfg->set_model_directory('.');
-	$cfg->set_connections(array(
-	//'development' => 'mysql://C344630_infantry:INFANTRY321@66.116.150.176/C344630_streetlight'));
-	//'development' => 'mysql://username:password@localhost/database_name'));
-	'development' => 'mysql://root@localhost/streetlight'));
-});
-	 
-class public_request extends ActiveRecord\Model
-{	
-	function generate_Reference()
-	{
-		$currentDateTime = getdate();
-		$year = substr($currentDateTime['year'],-2);
-		$month = $currentDateTime['mon'];
-		$day = $currentDateTime['mday'];
-		$hours = $currentDateTime['hours'];
-		$minutes = $currentDateTime['minutes'];
-		
-		$referenceNo = $year."".$month."".$day."".$hours."".$minutes;
-	
-		$existingRequest = public_request::find("reference_no = $referenceNo"); 
-		
-		While ($existingRequest != null)
-		{			
-			$currentDateTime = getdate();
-			$year = substr($currentDateTime['year'],-2);
-			$month = $currentDateTime['mon'];
-			$day = $currentDateTime['mday'];
-			$hours = $currentDateTime['hours'];
-			$minutes = $currentDateTime['minutes'];
-			$minutes++;
-			$referenceNo = $year."".$month."".$day."".$hours."".$minutes;
-
-			$existingRequest = public_request::find("reference_no = $referenceNo"); 
-		} 
-		
-		return $referenceNo;
-	}
-}
-
-$reference =  public_request::generate_Reference();
+$reference =  Public_Request::generate_Reference();
 
 echo $reference;
 ?>

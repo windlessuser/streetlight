ad68136879bd0571119325a4224f6359d03e54ce
unknown <st1@st1-PC.(none)>
2012-03-19 12:05:47 -0500

    managed to retrieve an array with the correct data

--- streetlight.php
+++ streetlight.php
@@ -4,7 +4,7 @@ require_once ('php-activerecord/ActiveRecord.php');
 ActiveRecord\Config::initialize(function($cfg) {
      $cfg->set_model_directory('models');
      $cfg->set_connections(array(
-         'development' => 'mysql://root:@localhost/streetlight')
+         'development' => 'mysql://root@localhost/streetlight')
          //'development' => 'mysql://C344630_infantry:INFANTRY321@66.116.150.176/C344630_streetlight')
 		//'development' => 'mysql://username:password@localhost/database_name'));
 		);
@@ -18,14 +18,16 @@ class Streetlight extends ActiveRecord\Model
 	
 	function retrieve_Streetlight($streetLightNo)
 	{
-		//$streetLight = Streetlight->find("streetlightid = $streetLightNo");
-		$streetLight =& $this->find_by_streetlightid($this->streetLightNo);  
+		//$posts = Post::find('all',array('conditions' => array('name=?','The Bridge Builder')));
+		$streetLight = Streetlight::find('all', array('conditions' => array('streetlight_no=?', $streetLightNo)));
+		//$streetLight =& parent::find_by_streetlight_no($this->streetLightNo);  
 		return $streetLight;
 	}
 }
 
+
 	$sl = new Streetlight;
-	echo "Streetlight: ".$sl->retrieve_Streetlight("101");
+	echo "Streetlight: ".print_r($sl->retrieve_Streetlight("101"));
 	
 	
 ?>

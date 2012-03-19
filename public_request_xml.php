7a9a33d31f7544955cb49eb3f733696ae24ab6d5
Marc Byfield <windlessuser@gmail.com>
2012-03-19 09:26:04 -0500

    see update 19-03-2012 - Marc

--- public_request_xml.php
+++ public_request_xml.php
@@ -1,7 +1,7 @@
 <?php
 require 'models/Public_Request2.php';
 
-$requests =& Public_Request::find('all');
+$requests =& Public_Request::find_all_by_public_request_statusid(1);
 
 header ("Content-Type:text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
@@ -9,4 +9,5 @@ echo "<Public_Requests>\n";
 foreach($requests as $request)
 	echo $request->to_xml(array('skip_instruct' =>true));
 echo '</Public_Requests>';
+
 ?>
